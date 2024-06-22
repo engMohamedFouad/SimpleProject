a6a81bc0507c6b7789c792261b5217cdc22ab5ac
Mohamed Fouad <103780066+engMohamedFouad@users.noreply.github.com>
2024-06-22 22:27:42 +0300

    RTL and LTR based on Culture (#7)
    * Added Localization With DataAnnotations Validations
    
    * Added LocalizableEntity
    Added Localization of Data
    
    * Added RTL and LTR based on Culture

---
+++ SimpleProject/Helpers/CultureHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleProject.Helpers
+{
+    public static class CultureHelper
+    {
+        public static bool IsRightToLeft()
+        {
+            return Thread.CurrentThread.CurrentCulture.TextInfo.IsRightToLeft;
+        }
+    }
+}
