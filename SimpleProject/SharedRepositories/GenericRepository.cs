6ec7f52531f2370553a4193648622966bc8f735c
Mohamed Fouad <103780066+engMohamedFouad@users.noreply.github.com>
2024-07-06 19:25:09 +0300

    Generic Repository (#9)
    * Added Localization With DataAnnotations Validations
    
    * Added LocalizableEntity
    Added Localization of Data
    
    * Added RTL and LTR based on Culture
    
    * Added the Update Product Changes
    
    * Added Remote Validation in Update Product
    
    * Add Generic Repository

---
+++ SimpleProject/SharedRepositories/GenericRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using SimpleProject.Data;
+
+namespace SimpleProject.SharedRepositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context=context;
+            _dbSet=_context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task DeleteRangeAsync(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Deletesync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<T> GetAsQueryable()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<T>> GetListAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task UpdateRangeAsync(IEnumerable<T> entities)
+        {
+            _dbSet.UpdateRange(entities);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Updatesync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
