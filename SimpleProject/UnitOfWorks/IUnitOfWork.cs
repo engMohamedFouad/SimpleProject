28fb9fc1d7a7a24c9bb9118a8bb370f206584c6f
Mohamed Fouad <103780066+engMohamedFouad@users.noreply.github.com>
2024-08-03 21:02:16 +0300

    Added UnitOfWork Files (#12)

---
+++ SimpleProject/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SimpleProject.SharedRepositories;
+
+namespace SimpleProject.UnitOfWorks
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : class;
+        public Task<IDbContextTransaction> BeginTransactionAsync();
+        public Task CommitTransactionAsync();
+        public Task RollbackTransactionAsync();
+        Task<int> CompleteAsync();
+    }
+}
---
+++ SimpleProject/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using SimpleProject.Data;
+using SimpleProject.SharedRepositories;
+
+namespace SimpleProject.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            await _context.Database.CommitTransactionAsync();
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IGenericRepository<T> Repository<T>() where T : class
+        {
+            if (_repositories.ContainsKey(typeof(T)))
+            {
+                return _repositories[typeof(T)] as IGenericRepository<T>;
+            }
+            var repository = new GenericRepository<T>(_context);
+            _repositories[typeof(T)] = repository;
+            return repository;
+
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+    }
+}
